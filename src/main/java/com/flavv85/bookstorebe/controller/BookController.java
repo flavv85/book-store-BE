6cda8a7c2d1e3d55ff98120643070dbe088a8187
FlaviusGorgan <gorganflavius@gmail.com>
2021-06-09 22:34:26 +0300

    uploadImd_addBook

--- src/main/java/com/flavv85/bookstorebe/controller/BookController.java
+++ src/main/java/com/flavv85/bookstorebe/controller/BookController.java
@@ -2,12 +2,12 @@ package com.flavv85.bookstorebe.controller;
 
 import com.flavv85.bookstorebe.model.Book;
 import com.flavv85.bookstorebe.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,15 +15,27 @@ import java.util.List;
 @RequestMapping(path = "books")
 public class BookController {
 
-    private final BookRepository bookRepository;
+    private byte[] bytes;
 
-    public BookController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
+    @Autowired
+    private BookRepository bookRepository;
 
     @GetMapping("/get")
-    public List<Book> getBooks(){
+    public List<Book> getBooks() {
         return bookRepository.findAll(Sort.by(Sort.DEFAULT_DIRECTION, "name"));
     }
 
+    // image bytes for the books
+    @PostMapping("/upload")
+    public void uploadImage(@RequestParam("imageFile") MultipartFile file) throws IOException {
+        this.bytes = file.getBytes();
+    }
+
+    // save book details
+    @PostMapping("/add")
+    public void createBook(@RequestBody Book book) throws IOException {
+        book.setPicByte(this.bytes);
+        bookRepository.save(book);
+        this.bytes = null;
+    }
 }
